3dc309d8a7db890ef85259fbecd41dcbbb18e8cd
Chad <Mapletonkid@gmail.com>
2020-06-05 09:08:42 -0600

    Ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,20 @@ def has_negatives(a):
     """
     # Your code here
 
+    result = []
+    num_hash = {}
+
+    for num in a:
+        fixNum = abs(num)
+
+        if fixNum not in num_hash:
+            num_hash[fixNum] = 0
+        else:
+            num_hash[fixNum] += 1
+        
+        if num_hash[fixNum] > 0:
+            result.append(fixNum)
+
     return result
 
 
